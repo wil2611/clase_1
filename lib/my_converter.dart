a407332b9a86e779df544e13dcaa725fdddaf980
William Perez <73206273+wil2611@users.noreply.github.com>
2023-08-02 11:15:34 -0500

    Update my_converter.dart

--- lib/my_converter.dart
+++ lib/my_converter.dart
@@ -3,7 +3,7 @@ import 'Utils.dart';
 class MyConverter {
   // todo: Define dos varibles late privadas String [_binary] y [_decimal]
   late String _binary;
-  late String _decimal
+  late String _decimal;
   MyConverter() {
     // todo: Inicializa [_binary] y [_decimal] en ['0']
     _binary='0';
